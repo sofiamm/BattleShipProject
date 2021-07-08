bbde85a185f3069d2d2201b75f3fd4243a1a33ed
Sofía Mora Muñoz <sofiamoramunoz6@gmail.com>
2021-07-07 21:17:33 -0600

    Create Barcos.java

---
+++ src/Barcos.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jhon
+ */
+public class Barcos {
+    public int cantidadDeNaves;
+    public int posiciones;
+    public String ataque;
+    
+    public void conteoDeNaves(){}
+    public void posicionarNaves(){}
+    public void dirigirAtaques(){}
+    
+}
