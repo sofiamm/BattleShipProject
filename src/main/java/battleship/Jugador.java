11ed57f91df810107060cd7966195d394e0dbefe
Sofía Mora Muñoz <sofiamoramunoz6@gmail.com>
2021-07-29 19:04:21 -0600

    New data

---
+++ src/main/java/battleship/Jugador.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship;
+
+/**
+ *
+ * @author Jhon
+ */
+//Atributos de judador
+public class Jugador {
+    public String nombre;
+    public String apellido;
+    public int edad;
+    public String correoElectronico;
+    
+    public void escogerNaves (){}
+    public void seleccionarTablero(){}
+    public void insertarDatos(){}
+    
+    
+}
